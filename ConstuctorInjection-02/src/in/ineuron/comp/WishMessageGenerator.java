eaff03de3f7b635668c6c59ef3d05980e6fda344
devavratwadekar <devavrat.wadekar@gmail.com>
2023-06-13 04:56:38 +0530

    Constuctor Injection using Core Java, using Bean Factory ,using ApplicationContext

--- ConstuctorInjection-02/src/in/ineuron/comp/WishMessageGenerator.java
+++ ConstuctorInjection-02/src/in/ineuron/comp/WishMessageGenerator.java
@@ -11,6 +11,10 @@ public class WishMessageGenerator {
 	static {
 		System.out.println("WishMessageGenerator.class file is loading...");
 	}
+	
+	public WishMessageGenerator() {
+		System.out.println("WishMessageGenerator :: Zero param constructor..");
+	}
 
 	public WishMessageGenerator(Date date) {
 		this.date = date;
