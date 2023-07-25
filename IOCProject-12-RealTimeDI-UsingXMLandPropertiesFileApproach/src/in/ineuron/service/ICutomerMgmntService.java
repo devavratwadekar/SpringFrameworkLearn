d3780792f946d5002d8267b596ba13234e010168
devavratwadekar <devavrat.wadekar@gmail.com>
2023-07-25 20:08:54 +0530

    connectionProperties[key,value] update in xml file commit

---
+++ IOCProject-12-RealTimeDI-UsingXMLandPropertiesFileApproach/src/in/ineuron/service/ICutomerMgmntService.java
@@ -0,0 +1,9 @@
+package in.ineuron.service;
+
+import in.ineuron.dto.CustomerDTO;
+
+public interface ICutomerMgmntService {
+
+	public String claculateSimpleInterest(CustomerDTO dto) throws Exception;
+
+}
