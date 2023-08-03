91c4b0474134a3dbc7e366b1ac0731fd6ce80365
devavratwadekar <devavrat.wadekar@gmail.com>
2023-08-03 20:09:33 +0530

    P-namespace and C-namespace Example commit

---
+++ IOCProject-21-SpringPnameSpaceandCnameSpace/src/in/ineuron/comp/Department.java
@@ -0,0 +1,21 @@
+package in.ineuron.comp;
+
+import java.util.Date;
+
+public class Department {
+	private int dno;
+	private String dname;
+	private Date doj;
+
+	public Department(int dno, String dname, Date doj) {
+		this.dno = dno;
+		this.dname = dname;
+		this.doj = doj;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [dno=" + dno + ", dname=" + dname + ", doj=" + doj + "]";
+	}
+
+}
--- IOCProject-21-SpringPnameSpaceandCnameSpace/src/in/ineuron/test/ClientApp.java
+++ IOCProject-21-SpringPnameSpaceandCnameSpace/src/in/ineuron/test/ClientApp.java
@@ -1,24 +1,19 @@
 package in.ineuron.test;
 
-import java.util.Locale;
-
-//27 March 2023
+//28 March 2023
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import in.ineuron.comp.Employee;
+
 public class ClientApp {
 	public static void main(String[] args) throws Exception {
+		// Both Container(BeanFactory, ApplicationContext) supports P-namespace,C-namespace based programming.
 		ApplicationContext context = new ClassPathXmlApplicationContext("in/ineuron/cfg/applicationContext.xml");
 
-		Locale locale = new Locale("hi", "IN");
-		System.out.println(context.getMessage("btn1.cap", null, locale));
-		System.out.println(context.getMessage("btn2.cap", null, locale));
-
-		System.out.println();
-
-		System.out.println(context.getMessage("btn1.cap", new String[] { "Students" }, locale));
-		System.out.println(context.getMessage("btn1.cap", new String[] { "Teachers" }, locale));
+		Employee employee = context.getBean(Employee.class);
+		System.out.println(employee);
 
 		((AbstractApplicationContext) context).close();
 	}
