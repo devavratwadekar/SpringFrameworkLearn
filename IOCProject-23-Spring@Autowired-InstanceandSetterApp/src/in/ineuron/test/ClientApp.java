462dbf0155644847a34af4d65338ddccb8eb8d40
devavratwadekar <devavrat.wadekar@gmail.com>
2023-08-05 18:52:34 +0530

    @Autowired - Instance and Setter

---
+++ IOCProject-23-Spring@Autowired-InstanceandSetterApp/src/in/ineuron/test/ClientApp.java
@@ -0,0 +1,21 @@
+package in.ineuron.test;
+//28 March 2023
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.ineuron.comp.Flipkart;
+
+public class ClientApp {
+
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext factory = new ClassPathXmlApplicationContext("in/ineuron/cfg/applicationContext.xml");
+		
+		Flipkart flipkart = factory.getBean(Flipkart.class);
+		System.out.println(flipkart.doShopping(new String[] { "Apple", "Banana" }, new float[] { 120.0f, 90.0f }));
+
+		((AbstractApplicationContext) factory).close();
+
+	}
+}
